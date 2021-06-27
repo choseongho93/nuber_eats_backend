39076b2a518b15f16bc3c1c07d64fc869d8e3dcd
Nate <choseongho93@naver.com>
2021-06-27 20:00:11 +0900

    TypeORM Setup

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # nuber-eats-backend
 
-# install 
+# npm install 
 1. `npm i @nestjs/graphql graphql-tools graphql apollo-server-express`
 2. `npm i class-validator class-transformer`
+3. `npm i --save @nestjs/typeorm typeorm pg`
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    // autoSchemaFile: join(process.cwd(), 'src/schema.gql')
-    autoSchemaFile : true
+  imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'n210208',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot({
+     autoSchemaFile : true
   }
-  ), RestaurantsModule],
+  )],
   controllers: [],
   providers: [ ],
 })
