e5fccb84291c69906ea690cf43713e31a5533e90
Nate <choseongho93@naver.com>
2021-06-27 17:10:06 +0900

    Arguments complete

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entities';
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(retrurns=> Restaurant)
-  myRestaurant(){
-    return true;
+  @Query(retrurns=> [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[]{
+    console.log(veganOnly);
+    return [];
   }
 }
