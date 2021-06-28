152034119aad8ebfea86f1dd5bbcba9d9abffea4
nate <choseongho93@naver.com>
2021-06-28 22:29:15 +0900

    Our First Entity

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { createRestaurantDto } from './dtos/create-restaurant.dto';
-import { Restaurant } from './entities/restaurants.entities';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
