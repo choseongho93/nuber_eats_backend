85cd3b7033fbd48a6631ba3fbca8bb0764e199ca
Nate <choseongho93@naver.com>
2021-07-04 19:52:33 +0900

    Mapped Types

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,31 +1,38 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
-export class Restaurant{
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    @Field(type=>Number)
-    id: number;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
 
-    @Field(type=>String)
-    @Column()
-    name: string;
+  @Field(type => Boolean)
+  @Column()
+  @IsBoolean()
+  isVegan: boolean;
 
-    @Field(type=>Boolean)
-    @Column()
-    isVegan: boolean;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  address: string;
 
-    @Field(type => String)
-    @Column()
-    address: string;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  ownersName: string;
 
-    @Field(type=>String)
-    @Column()
-    ownerName:string;
-
-    @Field(type => String)
-    @Column()
-    categoryName:string;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  categoryName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,25 +1,23 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
-import { RestaurantService } from './restaurant.service';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
-  constructor(private readonly restaurantService: RestaurantService){}
-
-  @Query(retrurns=> [Restaurant])
-  restaurants(): Promise<Restaurant[]>{
+  constructor(private readonly restaurantService: RestaurantService) {}
+  @Query(returns => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getAll();
   }
-  
   @Mutation(returns => Boolean)
   async createRestaurant(
-    @Args() createRestaurantDto: createRestaurantDto,
-  ): Promise<Boolean>{
-    try{
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    try {
       await this.restaurantService.createRestaurant(createRestaurantDto);
       return true;
-    }catch(e){
+    } catch (e) {
       console.log(e);
       return false;
     }
