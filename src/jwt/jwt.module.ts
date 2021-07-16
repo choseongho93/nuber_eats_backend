65bc3f0dc94597748b58fc666bbe337b1fb8d2a4
nate <choseongho93@naver.com>
2021-07-16 17:36:22 +0900

    JWT Module part Two

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,4 +1,6 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
 import { JwtService } from './jwt.service';
 
 @Module({
@@ -6,11 +8,17 @@ import { JwtService } from './jwt.service';
 })
 @Global()
 export class JwtModule {
-    static forRoot(): DynamicModule {
+    static forRoot(options: JwtModuleOptions): DynamicModule {
         return{
             module:JwtModule,
             exports: [JwtService],
-            providers: [JwtService]
+            providers: [
+                {
+                    provide: CONFIG_OPTIONS, //provide 변수 이름 
+                    useValue: options, // PRIVATE_KEY 값
+                },
+                JwtService,
+            ]
         }
     }
 }
