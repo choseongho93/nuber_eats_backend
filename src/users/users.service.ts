d7abf8499a1d5540d9d29a021b73fa3d171cd63f
nate <choseongho93@naver.com>
2021-07-25 13:25:01 +0900

    updateProfile part Three

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,66 +9,76 @@ import { EditProfileInput } from './dtos/edit-profile.dto';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User) private readonly users: Repository<User>,
-    private readonly jwtService: JwtService,
-  ) {}
+    constructor(
+        @InjectRepository(User) private readonly users: Repository<User>,
+        private readonly jwtService: JwtService,
+    ) {}
 
-  async createAccount({
-    email,
-    password,
-    role,
-  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
-    try {
-      const exists = await this.users.findOne({ email });
-      if (exists) {
-        return { ok: false, error: 'There is a user with that email already' };
-      }
-      await this.users.save(this.users.create({ email, password, role }));
-      return { ok: true };
-    } catch (e) {
-      return { ok: false, error: "Couldn't create account" };
+    async createAccount({
+        email,
+        password,
+        role,
+    }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+        try {
+            const exists = await this.users.findOne({ email });
+            if (exists) {
+                return {
+                    ok: false,
+                    error: 'There is a user with that email already',
+                };
+            }
+            await this.users.save(this.users.create({ email, password, role }));
+            return { ok: true };
+        } catch (e) {
+            return { ok: false, error: "Couldn't create account" };
+        }
     }
-  }
 
-  async login({
-    email,
-    password,
-  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
-    // make a JWT and give it to the user
-    try {
-      const user = await this.users.findOne({ email });
-      if (!user) {
-        return {
-          ok: false,
-          error: 'User not found',
-        };
-      }
-      const passwordCorrect = await user.checkPassword(password);
-      if (!passwordCorrect) {
-        return {
-          ok: false,
-          error: 'Wrong password',
-        };
-      }
-      const token = this.jwtService.sign(user.id);
-      return {
-        ok: true,
-        token,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error,
-      };
+    async login({
+        email,
+        password,
+    }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+        // make a JWT and give it to the user
+        try {
+            const user = await this.users.findOne({ email });
+            if (!user) {
+                return {
+                    ok: false,
+                    error: 'User not found',
+                };
+            }
+            const passwordCorrect = await user.checkPassword(password);
+            if (!passwordCorrect) {
+                return {
+                    ok: false,
+                    error: 'Wrong password',
+                };
+            }
+            const token = this.jwtService.sign(user.id);
+            return {
+                ok: true,
+                token,
+            };
+        } catch (error) {
+            return {
+                ok: false,
+                error,
+            };
+        }
     }
-  }
 
-  async findById(id: number): Promise<User> {
-    return this.users.findOne({ id });
-  }
+    async findById(id: number): Promise<User> {
+        return this.users.findOne({ id });
+    }
 
-  async editProfile(userId: number, {email, password}: EditProfileInput){
-    return this.users.update(userId, {email, password});
-  }
+    async editProfile(userId: number, {email, password}: EditProfileInput) {
+      const user = await this.users.findOne(userId);
+      if(email){
+        user.email = email;
+      }
+      if(password){
+        user.password = password;
+      }
+      return this.users.save(user);
+    }
 }
