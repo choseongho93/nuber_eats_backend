1d39e7229049400a6c3df9786e26b2c4656d1fa4
nate <choseongho93@naver.com>
2021-08-15 17:21:34 +0900

    Writing Our First Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,63 +1,84 @@
-import { Test } from "@nestjs/testing";
-import { getRepositoryToken } from "@nestjs/typeorm";
-import { JwtService } from "src/jwt/jwt.service";
-import { MailService } from "src/mail/mail.service";
-import { User } from "./entities/user.entity";
-import { Verification } from "./entities/verification.entity";
-import { UserService } from "./users.service"
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+import { UserService } from './users.service';
 
 const mockRepository = {
-    findeOne: jest.fn(),
-    save: jest.fn(),
-    create: jest.fn(),
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
 };
 
 const mockJwtService = {
-    sign: jest.fn(),
-    verify: jest.fn(),
+  sign: jest.fn(),
+  verify: jest.fn(),
 };
 
 const mockMailService = {
-    sendVerificationEmail: jest.fn(),
-}
+  sendVerificationEmail: jest.fn(),
+};
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UserService', () => {
-    let service: UserService;
+  let service: UserService;
+  let usersRepository: MockRepository<User>;
 
-    beforeAll(async () => {
-        const module = await Test.createTestingModule({
-            providers: [
-                UserService,
-                {
-                    provide: getRepositoryToken(User),
-                    useValue: mockRepository,
-                },
-                {
-                    provide: getRepositoryToken(Verification),
-                    useValue: mockRepository,
-                },
-                {
-                    provide: JwtService,
-                    useValue: mockJwtService,
-                },
-                {
-                    provide: MailService,
-                    useValue: mockMailService,
-                }
-            ],
-        }).compile();
-        service = module.get<UserService>(UserService);
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+    service = module.get<UserService>(UserService);
+    usersRepository = module.get(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('createAccount', () => {
+    it('should fail if user exists', async () => {
+      usersRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: 'hghgh',
+      });
+      const result = await service.createAccount({
+        email: '',
+        password: '',
+        role: 0,
+      });
+      console.log(result);
+      expect(result).toMatchObject({
+        ok: false,
+        error: 'There is a user with that email already',
+      });
     });
-    
+  });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
-    })
-
-    it.todo('createAccount');
-    it.todo('login');
-    it.todo('findById');
-    it.todo('editProfile');
-    it.todo('verifyEmail');
+  it.todo('login');
+  it.todo('findById');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
 });
-
